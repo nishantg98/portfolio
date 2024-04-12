e554c27b1c97e0f1291d58094e070c50dede3482
Nishant Gupta <68242561+nishantg98@users.noreply.github.com>
2024-04-12 10:12:32 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -35,18 +35,18 @@ const CONFIG = {
         {
           title: '',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            '',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            '',
+          link: '',
         },
         {
           title: '',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            '',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            '',
+          link: '',
         },
       ],
     },
@@ -101,19 +101,19 @@ const CONFIG = {
       position: 'Associate DevOps Engineer',
       from: 'March 2024',
       to: 'Present',
-      companyLink: 'https://successive.tech/',
+      companyLink: 'https://successive.tech',
     },
     {
       company: 'Successive Digital',
       position: 'Associate DevOps Engineer',
       from: 'October 2020',
       to: 'August 2022',
-      companyLink: 'https://successive.tech/',
+      companyLink: 'https://successive.tech',
     },
   ],
   certifications: [
     {
-      name: 'Microsoft Azure Fundamentals',
+      name: 'Microsoft Azure Fundamentals (AZ-900)',
       body: '',
       year: 'March 2023',
       link: '',
